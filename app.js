88d3ea8c246cc5d00c1d4911d4715256bf0187a7
SJB7788 <83483363+SJB7788@users.noreply.github.com>
2024-09-08 23:58:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static("/js"));
-app.use(express.static("/css"));
+app.use(express.static(path.join(__dirname, 'js')));
+app.use(express.static(path.join(__dirname, 'css')));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
