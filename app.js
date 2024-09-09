6d598ba7cd179a4b527e1efc0ea5a0ff168b77da
SJB7788 <83483363+SJB7788@users.noreply.github.com>
2024-09-09 00:12:10 -0700

    asdadsad

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'js')));
-app.use(express.static(path.join(__dirname, 'css')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const port = process.env.PORT || 8080;
