4509283df6c403c10a8bc921168798a52a2ba38d
SJB7788 <83483363+SJB7788@users.noreply.github.com>
2024-09-13 17:02:14 -0700

    lab 02 prep

--- public/js/index.js
+++ public/js/index.js
@@ -1,141 +1,68 @@
-class MemoryGame {
-  constructor(buttonCount) {
-    this.container = document.getElementById("game_container");
-
-    this.buttonClicked = [];
-    this.buttonArray = [];
-    for (let count = 0; count < buttonCount; count++) {
-      const buttonText = count + 1;
-      const randomColor =
-        "#" + Math.floor(Math.random() * 16777215).toString(16);
-      const button = new Button(buttonText, randomColor, this.container);
-      button.button.className = "game_content";
-      button.button.addEventListener("click", () => {
-        this.handleClick(count);
-      });
-      this.buttonArray.push(button);
-    }
-
-    this.gameState = false;
-    this.handleGameStart();
+class Notepad {
+  constructor(container) {
+    this.notepadContainer = container
+    this.createNoteButton = 
   }
 
-  handleClick(index) {
-    if (this.gameState) {
-      const currentButton = this.buttonArray[index];
-      if (this.buttonClicked.length < this.buttonArray.length) {
-        this.buttonClicked.push(currentButton);
-
-        if (
-          this.buttonArray[this.buttonClicked.length - 1].text !=
-          currentButton.text
-        ) {
-          this.gameState = false;
-          this.handleGameOver(false);
-        }
-      }
-
-      if (this.buttonClicked.length === this.buttonArray.length) {
-        this.gameState = false;
-        this.handleGameOver(true);
-      }
-    }
+  createNote(text) {
+    // create new note object which already has button and noteRectangle populated 
+    const note = new Note(text);
+    // append the note to notepadContainer
+    this.notepadContainer.appendChild(note);
   }
 
-  handleGameStart() {
-    this.gameState = true;
-    setTimeout(() => {
-      this.scrambleButtons(this.buttonArray);
-    }, this.buttonArray.length * 1000);
+}
+
+class Note {
+  constructor(text) {
+    // create new noteContainer
+    this.noteContainer = document.createElement("div");
+    this.noteContainer.className = "note_container";
+
+    // create a noteRectangle and Button object
+    this.note = new NoteRectangle(text);
+    this.button = new Button();
+
+    // add event listener to remove the note container 
+    this.button.getButton().addEventListener('click', () => this.removeNote());
+
+    // append the button and noteRectangle object to noteContainer div
+    this.noteContainer.appendChild(this.note.getNoteElement());
+    this.noteContainer.appendChild(this.button.getButton());
   }
 
-  handleGameOver(win) {
-    const message = document.createElement("h1");
-    message.className = "game_content";
-    message.className += " game_message";
-    if (win) {
-      message.innerHTML = "Excellent memory!";
-    } else {
-      message.innerHTML = "Wrong order!";
-    }
-    this.container.appendChild(message);
+  removeNote(container) {
+    container.removeChild(this.noteContainer);
   }
 
-  scrambleButtons(buttonArray) {
-    let scrambleCount = 0;
+  getNoteContainer() {
+    return this.noteContainer;
+  }
+}
 
-    const scrambleInterval = setInterval(() => {
-      if (scrambleCount >= 3) {
-        clearInterval(scrambleInterval);
-        buttonArray.forEach((button) => {
-          button.hideText();
-          button.button.disabled = false;
-        });
-        return;
-      }
+class NoteRectangle {
+  constructor(text) {
+    this.text = text;
 
-      buttonArray.forEach((button) => {
-        const containerWidth = window.innerHeight;
-        const containerHeight = window.innerWidth;
+    this.noteElement = document.createElement("div"); 
+    this.noteElement.style.width = "10em";
+    this.noteElement.style.height = "5em";
+    this.textElement = document.createElement("p");
+    this.textElement.innerHTML = this.text;
+  }
 
-        const randomTop = Math.random() * (containerHeight - 74);
-        const randomLeft = Math.random() * (containerWidth - 144);
-        const top = `${randomTop}px`;
-        const left = `${randomLeft}px`;
-
-        button.button.style.position = "absolute";
-        button.moveX(left);
-        button.moveY(top);
-      });
-
-      scrambleCount++;
-    }, 2000);
+  getNoteElement() {
+    return this.noteElement;
   }
 }
 
 class Button {
-  constructor(text, color, container) {
+  constructor() {
     this.button = document.createElement("button");
-    this.text = text;
-    this.button.innerHTML = text;
-
-    this.button.style.backgroundColor = color;
-    this.button.style.color = "black";
-    this.button.style.fontSize = "1em";
-    this.button.style.height = "5em";
-    this.button.style.width = "10em";
-    this.button.style.top = "0px";
-    this.button.disabled = true;
-    container.appendChild(this.button);
+    this.button.innerHTML = "remove";
   }
 
-  moveX(x) {
-    this.button.style.top = x;
-  }
-
-  moveY(y) {
-    this.button.style.left = y;
-  }
-
-  hideText() {
-    this.button.innerHTML = "";
-  }
-
-  showText() {
-    this.button.innerHTML = this.text;
+  getButton() {
+    return this.button;
   }
 }
-
-const inputBox = document.getElementById("ball_input");
-const startButton = document.getElementById("go_button");
-startButton.addEventListener("click", () => {
-  const allButtons = document.querySelectorAll(".game_content");
-  if (allButtons) {
-    allButtons.forEach((button) => {
-      button.remove();
-    });
-  }
-  const numberOfButtons = parseInt(inputBox.value);
-  const game = new MemoryGame(numberOfButtons);
-  game.gameState = true;
-});
