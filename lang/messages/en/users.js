ffa7b909efb65984e6f738ce6085e20d6d642912
SJB7788 <83483363+SJB7788@users.noreply.github.com>
2024-09-19 22:04:18 -0700

    Update users.js

---
+++ lang/messages/en/users.js
@@ -0,0 +1,20 @@
+// index.html
+export const indexPage = {
+    title: "Lab 1 Main Page",
+    aReader: "Reader",
+    aWriter: "Writer"
+}
+
+// reader.html
+export const readerPage = {
+    lastUpdated: "Last Updated: "
+}
+
+// writer.html
+export const writerPage = {
+    refresh: "Refresh",
+    add: "Add"
+}
+
+
+
